18a48d7609b981d7fa5a327fb5e8e495f1371e5c
alsajo <alexissamanthajohnson@gmail.com>
2021-03-24 10:59:34 -0500

    added second javascript to display a button

--- style.js
+++ style.js
@@ -5,3 +5,11 @@ function randonumber(x,y){
  // Command for html to call javascript into code
  //  <script src="style.js"></script>
  // return command means value has use later
+ 
+ var txt;
+var r = confirm("Press a button!");
+if (r == true) {
+  txt = "You pressed OK!";
+} else {
+  txt = "You pressed Cancel!";
+}
